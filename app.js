89b7b893a936c067d5dfdb6b8ebf2fa03d421b22
Suprasanna Ojha <ssojha95@gmail.com>
2026-01-21 10:54:10 +0200

    env

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const port = 3000;
 
 app.get("/", (req, res) => {
   res.send(
-    "Hello World! This is a basic Express server running on my own server.",
+    `Hello World! This is a basic Express server running on my own server and environment variable is: ${process.env.DEMO_ENV_VAR}`,
   );
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+  console.log("Env value is:", process.env.DEMO_ENV_VAR);
 });
